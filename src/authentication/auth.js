13d62adaee3e20bc7a1b741bb03cac015180cc49
dhavaljethava161 <125587725+dhavaljethava161@users.noreply.github.com>
2023-10-13 12:11:25 +0530

    all in one auth

--- src/authentication/auth.js
+++ src/authentication/auth.js
@@ -1,58 +1,6 @@
 import jwt from "jsonwebtoken";
 import { models } from "../models";
 
-export const studentAuth = async (req, res, next) => {
-  try {
-    const token = req?.headers?.authorization?.split(" ")?.[1];
-
-    const data = jwt.verify(token, process.env.AUTH_KEY);
-
-    const userData = await models.User.findOne({
-      email: data.email,
-      userType: data.userType,
-    });
-
-    req.loginUser = userData;
-
-    if (data) next();
-    else res.send({ status: 400, err: "Please sign in first" });
-  } catch (error) {
-    res.send({ status: 400, error: error.message });
-  }
-};
-//TODO make common auth for principle and teacher for get fees
-export const teacherAuth = async (req, res, next) => {
-  const token = req?.headers?.authorization?.split(" ")?.[1];
-
-  const data = jwt.verify(token, process.env.AUTH_KEY);
-  if (!data) res.send("Please signIn first");
-
-  const userData = await models.User.findOne({
-    email: data.email,
-    userType: data.userType,
-  });
-
-  req.loginUser = userData;
-  if (data.userType !== "teacher") res.send("You are'nt Teacher");
-  else next();
-};
-
-export const principleAuth = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-
-    const data = jwt.verify(token, process.env.AUTH_KEY);
-    if (!data) res.send("Please signIn first");
-
-    const userData = await models.User.findOne({ email: data.email });
-    req.loginUser = userData;
-    if (userData.userType !== "principle") res.send("You are'nt principle");
-    else next();
-  } catch (error) {
-    res.send({ status: 400, error: error.message });
-  }
-};
-
 export const checkAuth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
