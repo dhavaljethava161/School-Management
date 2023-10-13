21dad18ef9a747304f0696f758dece913a6ef078
dhavaljethava161 <125587725+dhavaljethava161@users.noreply.github.com>
2023-10-13 19:57:35 +0530

    created principle one query

--- src/database/default.js
+++ src/database/default.js
@@ -3,18 +3,19 @@ import { config } from "../config";
 
 export async function admin() {
   try {
-    const findPrinciple = await models.User.findOne({
-      email: process.env.PRINCIPLE_MAIL,
-    });
-    if (!findPrinciple) {
-      models.User.create({
-        email: config.admin_cred.PRINCIPLE_MAIL,
-        password: config.admin_cred.PRINCIPLE_PASS,
-        number: config.admin_cred.PRINCIPLE_NUM,
-        userType: "principle",
-      });
-      console.log("Principle created...!");
-    }
+    await models.User.updateOne(
+      { email: process.env.PRINCIPLE_MAIL },
+      {
+        $set: {
+          email: config.admin_cred.PRINCIPLE_MAIL,
+          password: config.admin_cred.PRINCIPLE_PASS,
+          number: config.admin_cred.PRINCIPLE_NUM,
+          userType: "principle",
+          verified: true,
+        },
+      },
+      { upsert: true, new: true }
+    );
   } catch (error) {
     console.log(error.message);
   }
